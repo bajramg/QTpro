46e69118fb9ce2f13652aee4404ef7ff9363f30a
bajramg <bajramg@gmail.com>
2019-02-03 15:00:32 +0200

    Add files via upload

---
+++ Demo1/src/controller/formcontroller.cpp
@@ -0,0 +1,34 @@
+/**
+  @file
+  @author Stefan Frings
+*/
+
+#include "formcontroller.h"
+
+FormController::FormController()
+{
+}
+
+void FormController::service(HttpRequest& request, HttpResponse& response)
+{
+    response.setHeader("Content-Type", "text/html; charset=ISO-8859-1");
+    if (request.getParameter("action") == "show") {
+        response.write("<html><body>");
+        //response.write("Juttuja ");
+
+        response.write("Name = ");
+        response.write(request.getParameter("name"));
+        response.write("<br>City = ");
+        response.write(request.getParameter("city"));
+        response.write("</body></html>", true);
+    } else {
+        response.write("<html><body>");
+        response.write("<form method=\"post\">");
+        response.write("  <input type=\"hidden\" name=\"action\" value=\"show\">");
+        response.write("  Tunnus  : <input type=\"text\" name=\"name\"><br>");
+        response.write("  Salasana: <input type=\"text\" name=\"city\"><br>");
+        response.write("  <input type=\"submit\">");
+        response.write("</form>");
+        response.write("</body></html>", true);
+    }
+}
